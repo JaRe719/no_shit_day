24f14739e615434da731803614252423795dbd27
René Behrens <rene.behrens@aol.com>
2024-07-16 23:35:34 +0200

    update style on GameOver.scss

--- frontend/src/pages/GameOver/GameOver.jsx
+++ frontend/src/pages/GameOver/GameOver.jsx
@@ -4,7 +4,7 @@ import "./GameOver.scss";
 
 const GameOver = () => {
   return (
-    <section>
+    <section id="gameOver">
       <div className="textBox">
         <h1>Shit happens</h1>
         <h2>Das war wohl ein Schuss in den Porzellanofen!</h2>
